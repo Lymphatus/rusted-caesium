63a4618bc3cb973761339ebb4afd95449ab40916
Matteo Paonessa <matteo.paonessa@lastminute.com>
2019-11-11 14:39:59 +0100

    Magic happens ✨

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@ extern crate libc;
 
 use std::os::raw::c_char;
 use std::ffi::CString;
+use std::env;
 
 #[repr(C)]
+#[derive(Debug)]
 struct CsJpegPars
 {
     quality: i32,
     exif_copy: bool,
     dct_method: i32,
-    scale_factor: f32,
+    scale_factor: f64,
 }
 
 #[repr(C)]
+#[derive(Debug)]
 struct CsPngPars
 {
     iterations: i32,
@@ -20,11 +23,12 @@ struct CsPngPars
     block_split_strategy: i32,
     lossy_8: bool,
     transparent: bool,
-    auto_filter_strategy: u32,
-    scale_factor: f32,
+    auto_filter_strategy: bool,
+    scale_factor: f64,
 }
 
 #[repr(C)]
+#[derive(Debug)]
 struct CsImagePars
 {
     jpeg: CsJpegPars,
@@ -37,20 +41,25 @@ extern {
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let input = args[1].as_bytes();
+    let output = args[2].as_bytes();
+
     let jpeg_pars = CsJpegPars {
-        quality: 80,
+        quality: 0,
         exif_copy: false,
         dct_method: 4,
         scale_factor: 1.0,
     };
 
     let png_pars = CsPngPars{
-        iterations: 10,
-        iterations_large: 5,
+        iterations: 2,
+        iterations_large: 1,
         block_split_strategy: 4,
         lossy_8: true,
         transparent: true,
-        auto_filter_strategy: 0,
+        auto_filter_strategy: false,
         scale_factor: 1.0
     };
 
@@ -59,11 +68,13 @@ fn main() {
         png: png_pars
     };
 
-    let origin_str = CString::new(b"/Users/lymphatus/Pictures/Tanzania POST/IMG_1278.jpg" as &[u8]).unwrap();
+    let origin_str = CString::new(input).unwrap();
     let origin: *const c_char = origin_str.as_ptr();
-    let destination_str = CString::new(b"/Users/lymphatus/Pictures/hydroxide_test/IMG_1278.jpg" as &[u8]).unwrap();
+    let destination_str = CString::new(output).unwrap();
     let destination: *const c_char = destination_str.as_ptr();
 
-
-    unsafe { cs_compress(origin, destination, cs_pars); }
+    unsafe {
+        // let cs_pars: CsImagePars = initialize_parameters();
+        cs_compress(origin, destination, cs_pars);
+    }
 }
