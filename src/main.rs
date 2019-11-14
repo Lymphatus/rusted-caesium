df5090ded7fafa060e334141be4af11b17e26e62
Matteo Paonessa <matteo.paonessa@lastminute.com>
2019-11-14 15:00:49 +0100

    Moved pars to a module

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,11 @@ use structopt::StructOpt;
 use std::os::raw::c_char;
 use std::ffi::CString;
 
-#[repr(C)]
-struct CsJpegPars
-{
-    quality: i32,
-    exif_copy: bool,
-    dct_method: i32,
-    scale_factor: f64,
-}
-
-#[repr(C)]
-struct CsPngPars
-{
-    iterations: i32,
-    iterations_large: i32,
-    block_split_strategy: i32,
-    lossy_8: bool,
-    transparent: bool,
-    auto_filter_strategy: bool,
-    scale_factor: f64,
-}
-
-#[repr(C)]
-struct CsImagePars
-{
-    jpeg: CsJpegPars,
-    png: CsPngPars,
-}
+mod cspars;
 
 #[link(name = "caesium")]
 extern {
-    fn cs_compress(origin: *const c_char, destination: *const c_char, pars: CsImagePars) -> bool;
+    fn cs_compress(origin: *const c_char, destination: *const c_char, pars: cspars::CsImagePars) -> bool;
 }
 
 #[derive(StructOpt, Debug)]
@@ -87,27 +61,8 @@ fn main() {
     let input = args.remove(0).into_os_string().into_string().unwrap();
     let output = opt.output.into_os_string().into_string().unwrap();
 
-    let jpeg_pars = CsJpegPars {
-        quality: opt.quality,
-        exif_copy: false,
-        dct_method: 4,
-        scale_factor: 1.0,
-    };
-
-    let png_pars = CsPngPars {
-        iterations: 2,
-        iterations_large: 1,
-        block_split_strategy: 4,
-        lossy_8: true,
-        transparent: true,
-        auto_filter_strategy: false,
-        scale_factor: 1.0,
-    };
-
-    let cs_pars = CsImagePars {
-        jpeg: jpeg_pars,
-        png: png_pars,
-    };
+    let mut cs_pars: cspars::CsImagePars = Default::default();
+    cs_pars.jpeg.quality = opt.quality;
 
     let origin_str = CString::new(input).unwrap();
     let origin: *const c_char = origin_str.as_ptr();
